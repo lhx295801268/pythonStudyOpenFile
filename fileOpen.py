e808d0eaee1e4ef9f719d2abe6b3bd4572f6db84
lhx295801268 <769681799@qq.com>
2018-07-25 17:28:25 +0800

    Add files via upload

--- fileOpen.py
+++ fileOpen.py
@@ -1,4 +1,5 @@
 import sys, getopt ,os , time , getpass, re
+# 打开文件 文件路径
 def openFile(filePath):
     isExist = os.path.exists(filePath);
     if False == isExist:
@@ -7,7 +8,7 @@ def openFile(filePath):
     fo = open(filePath);
     return fo;
 
-
+# 处理字符串
 def disposeStr(fileContent):
     if fileContent == None:
         return None;
@@ -19,8 +20,16 @@ def disposeStr(fileContent):
         matchStrLen = len(matchStr);
         #去掉首位空格
         line1.strip();
+        #将\r换行替换为\n换行
+        if (line1.find('\\r',0,len(line1)) >= 0) :
+            line1 = line1.replace('\\r','\\n',len(line1));
+        #将\r\n换行替换为\n换行
+        if (line1.find('\\r\\n',0,len(line1)) >= 0) :
+            line1 = line1.replace('\\r\\n','\\n',len(line1));
+
         midlleChildStrPos = line1.find(matchStr);
         lineLen = len(line1);
+
         if 0 > midlleChildStrPos :
             matchStr = '\"=\"';
             matchStrLen = len(matchStr);
@@ -30,8 +39,16 @@ def disposeStr(fileContent):
         if('\"' != line1[0]) or (midlleChildStrPos < 0):
             continue
         else :
-            preStr = line1[1:midlleChildStrPos];
-            annotationStr = line1[(midlleChildStrPos + matchStrLen):(len(line1) - 3)];
+            # "第一次出现的位置
+            firstShowPos = line1.find('\"');
+            firstShowPos += 1;
+            # 截取译前字符串
+            preStr = line1[firstShowPos:midlleChildStrPos];
+            # "最后一次出现的位置
+            suffixStrPos = line1.rfind('\"');
+            # 截取译后字符串
+            annotationStr = line1[(midlleChildStrPos + matchStrLen):(suffixStrPos)];
+            # 拼接标准字符串
             resultStr += ('\"V\":\"MCLocalization_' + str(lineCount) + '\",\"O\":\"' + preStr + '\",\"T\":\"' + annotationStr + '\"\n');
             lineCount += 1;
     return resultStr;
@@ -50,7 +67,7 @@ def matchSubstrPosList(srcStr, childStr):
         else:
             break;
     return resultList;
-
+# 将字符串写入文件
 def writeStrToFile(filePath, contentStr) :
     if None == contentStr or filePath == None :
         return NO;
@@ -58,7 +75,7 @@ def writeStrToFile(filePath, contentStr) :
     #写入文件;
     file.write(contentStr);
     file.close();
-
+# 程序文件所处路径
 curFilePath = os.getcwd();
 readFilePath = curFilePath + '/Localizable.strings';
 print('文件路径为：' + readFilePath);
