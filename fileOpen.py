4d6dc45eeb498bcc47512b24371ea7ddcc79244e
lhx295801268 <769681799@qq.com>
2018-07-25 11:16:25 +0800

    Add files via upload
    翻译文件转化完毕

--- fileOpen.py
+++ fileOpen.py
@@ -12,27 +12,58 @@ def disposeStr(fileContent):
     if fileContent == None:
         return None;
     resultStr = '';
+    
+    lineCount = 1;
     for line1 in fileContent :
-        if 0 > line1.find('" = "') :
-            continue;
+        matchStr = '\" = \"';
+        matchStrLen = len(matchStr);
+        #去掉首位空格
+        line1.strip();
+        midlleChildStrPos = line1.find(matchStr);
+        lineLen = len(line1);
+        if 0 > midlleChildStrPos :
+            matchStr = '\"=\"';
+            matchStrLen = len(matchStr);
+            midlleChildStrPos = line1.find(matchStr);
+
+        #只要首字符不是‘\"’或者没有找到对应字符子串的 直接continue 
+        if('\"' != line1[0]) or (midlleChildStrPos < 0):
+            continue
         else :
-            resultList = [];
-            resultList = matchSubstrPosList(line1, '"');
-            resultStr += '';
-        print('====>' + resultStr + '<====');
+            preStr = line1[1:midlleChildStrPos];
+            annotationStr = line1[(midlleChildStrPos + matchStrLen):(len(line1) - 3)];
+            resultStr += ('\"V\":\"MCLocalization_' + str(lineCount) + '\",\"O\":\"' + preStr + '\",\"T\":\"' + annotationStr + '\"\n');
+            lineCount += 1;
+    return resultStr;
 
 def matchSubstrPosList(srcStr, childStr):
     resultList = [];
     startIndex = 0;
     itemPos = -1;
+    srcStrLength = len(srcStr);
     while startIndex < len(srcStr) :
         itemPos = srcStr.find(childStr, startIndex, (len(srcStr) - startIndex));
         if itemPos >= 0 :
             # resultList += itemPos;
             resultList.append(itemPos);
             startIndex = itemPos + 1;
+        else:
+            break;
     return resultList;
 
-readFilePath = '/Users/xtkj20180625/Documents/Localizable.strings';
+def writeStrToFile(filePath, contentStr) :
+    if None == contentStr or filePath == None :
+        return NO;
+    file = open(filePath,'wr');
+    #写入文件;
+    file.write(contentStr);
+    file.close();
+
+curFilePath = os.getcwd();
+readFilePath = curFilePath + '/Localizable.strings';
+print('文件路径为：' + readFilePath);
 fileContent = openFile(readFilePath);
-disposeStr(fileContent);
+willWriteStr = disposeStr(fileContent);
+writeStrToFile((curFilePath + '/CKLocalizable'), willWriteStr);
+fileContent.close();
+# print('---->:\n'+willWriteStr+'\n');
